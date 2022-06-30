38f992943929070678e3387418ea8a4c42fcb5f3
Jaeyong <jaeyong2328@gmail.com>
2022-06-30 02:24:19 -0700

    submodule

--- onchain queries/py_graphql/test2.py
+++ onchain queries/py_graphql/test2.py
@@ -2,7 +2,7 @@ from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
 
 # Select your transport with a defined url endpoint
-transport = AIOHTTPTransport(url="https://api.studio.thegraph.com/query/28876/governance/v0.0.7")
+transport = AIOHTTPTransport(url="https://api.studio.thegraph.com/query/28876/governance/v0.2")
 
 # Create a GraphQL client using the defined transport
 client = Client(transport=transport, fetch_schema_from_transport=True)
@@ -11,16 +11,27 @@ client = Client(transport=transport, fetch_schema_from_transport=True)
 query = gql(
     """
     {
-      proposals(first: 100) {
-        id
-        proposalID
-        votes
-        voter
-      }
-    }
+        proposals(first: 100)
+        {
+            id
+            votes {
+              voter
+              single_vote
+            }
+        }
+    } 
     """
 )
 
 # Execute the query on the transport
-result = client.execute(query)
-print(result['proposals'])
+result = client.execute(query)['proposals']
+tmp = {}
+for proposal in result:
+    proposal_id = proposal['id']
+    max_vote = 0
+    for vote in proposal['votes']:
+        max_vote += int(vote['single_vote'])
+    voting_rate = max_vote / 10**23
+    tmp[proposal_id] = int(voting_rate)
+
+print(tmp)
