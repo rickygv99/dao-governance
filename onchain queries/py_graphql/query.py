a5ea3a753fe331677624444da36f29130cd322d1
Jaeyong <jaeyong2328@gmail.com>
2022-07-07 10:56:04 -0700

    generate compound voting rates

--- onchain queries/py_graphql/query.py
+++ onchain queries/py_graphql/query.py
@@ -20,6 +20,7 @@ query = gql(
               voter{
                 id
               }
+              single_vote
             }
         }
         singleVotes(first: 5, where: {support: 10}) {
@@ -39,8 +40,8 @@ query = gql(
                 proposalID{
                     id
                 }
-            support
-            single_vote
+                support
+                single_vote
             }
         }
         implementations{
@@ -55,7 +56,6 @@ query = gql(
 # Execute the query on the transport
 result = client.execute(query)
 
-voters = result['voters']
 
 # for voter in voters:
 #     n_voter = pd.json_normalize(voter['votes'])
@@ -64,35 +64,29 @@ voters = result['voters']
 #     break
 pd.set_option('display.max_colwidth', None)
 df_voters = pd.json_normalize(result,record_path=["voters", "votes"], meta=[['voters', 'id']])
-print(df_voters.sort_values('voters.id'))
 
 with open('compound_matrix.pickle', 'wb') as f:
     pickle.dump(df_voters, f)
 
-df = pd.DataFrame.from_dict(voters)
 # print(df)
 # print(len(result['voters']))
 
-# proposal_result = result['proposals']
-# implementation_result = result['implementations']
+proposal_result = result['proposals']
+implementation_result = result['implementations']
+tmp = {}
+for proposal in proposal_result:
+    proposal_id = proposal['id']
+    max_vote = 0
+    # tmp[proposal_id]['blocktime'] = proposal['blocktime']
+    for vote in proposal['votes']:
+        max_vote += int(vote['single_vote'])
+    voting_rate = max_vote / 10**25
+    tmp[proposal_id] = voting_rate
 # 
-# tmp = {}
-# for proposal in proposal_result:
-#     proposal_id = proposal['id']
-#     max_vote = 0
-#     tmp[proposal_id] = {}
-#     tmp[proposal_id]['blocktime'] = proposal['blocktime']
-#     for vote in proposal['votes']:
-#         max_vote += int(vote['single_vote'])
-#     voting_rate = max_vote / 10**25
-#     tmp[proposal_id]['vote_rate'] = voting_rate
-# 
-# print(tmp)
-# 
-# print(implementation_result)
-# 
-# with open('propsals.pickle', 'wb') as f:
-#     pickle.dump(tmp, f)
+df_voting_rates = pd.json_normalize(tmp)
+ 
+with open('propsals.pickle', 'wb') as f:
+    pickle.dump(df_voting_rates, f)
 # 
 # with open('implementations.pickle', 'wb') as f:
 #     pickle.dump(implementation_result, f)
